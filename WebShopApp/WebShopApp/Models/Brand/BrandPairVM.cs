9aae5f6809e082047729532a424807a4c9b56bc2
Radostina-Kostadinova <radostina.kostadinova06gmail.com>
2024-01-25 14:15:10 +0200

    proba

---
+++ WebShopApp/WebShopApp/Models/Brand/BrandPairVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebShopApp.Models.Brand
+{
+    public class BrandPairVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Brand")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ WebShopApp/WebShopApp/Models/Category/CategoryPairVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebShopApp.Models.Category
+{
+    public class CategoryPairVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Category")]
+        public string Name { get; set; } = null!;
+    }
+}
